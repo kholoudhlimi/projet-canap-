09560119114ac6d8eba25acf40e8b184ae3e1215
kholoudhlimi <kholud.hlimi@gmail.com>
2024-07-14 20:01:23 +0200

    mod

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,4 +1,4 @@
-const orderId = JSON.parse(localStorage.getItem("order")) || {};
+const orderId = JSON.parse(localStorage.getItem("order")) || [];
 const params = new URLSearchParams(window.location.search);
 const Id = params.get("id");
 const url = `http://localhost:3000/api/products/order/${Id}`;
